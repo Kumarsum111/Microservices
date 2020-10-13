eb328faabec91b61a6b2bb779d1e9f0e761b18fb
kumar <kumar@DESKTOP-U781Q5C>
2020-10-13 13:30:57 +0530

    movie-info-service 1st commit

---
+++ movie-info-service/src/main/java/com/sumit/MovieInfoServiceApplication.java
@@ -0,0 +1,23 @@
+package com.sumit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MovieInfoServiceApplication {
+	
+	@Bean          //only one instance is created (Singleton in nature)
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServiceApplication.class, args);
+	}
+
+}
